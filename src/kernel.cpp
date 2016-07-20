538ea077c3a0979f5fa0ab2d78a3dc5cb377bfec
T-Coin Devteam <tcoindev@gmail.com>
2016-07-21 00:25:07 +0800

    CheckPoint Added

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -15,20 +15,10 @@ typedef std::map<int, unsigned int> MapModifierCheckpoints;
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
         ( 0, 0xe00670b )
-        /*
         ( 1, 0x5189c78a )
-        ( 2, 0x38d937d2 )
-        ( 10, 0x9ea98337 )
-        ( 100, 0x085363a3 )
-        ( 1000, 0x6e8d7ad4 )
-        ( 2000, 0x021054de )
-        ( 3000, 0x90cb43b1 )
-        ( 3600, 0x4c352f08 )
-        ( 3601, 0x1302556e )
-        ( 3615, 0x34f3b4dd )
-        ( 3616, 0xa72b0d04 )
-        ( 3617, 0x990c8c5e )
-        */
+        ( 5, 0xafc293df )
+        ( 10, 0x9ce84843 )
+        ( 11, 0x2227fe8b )
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
